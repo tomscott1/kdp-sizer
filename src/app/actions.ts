e168a11883d160a736e6d5505f8bb32445a25c0a
Tom Scott <tomscott85@gmail.com>
2024-05-18 14:02:57 +1000

    remove reference to deleted file

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,7 +1,7 @@
-import { uploadFile } from "@/lib/process";
+// import { uploadFile } from "@/lib/process";
 
-export const handleFileUpload = async (file: File) => {
-    // Trigger the Server Action here (assuming you have a server action defined)
-    uploadFile(file); // Replace with your actual server action call
+// export const handleFileUpload = async (file: File) => {
+//     // Trigger the Server Action here (assuming you have a server action defined)
+//     uploadFile(file); // Replace with your actual server action call
 
-};
+// };
